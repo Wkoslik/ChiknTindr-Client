42d2000deca83c8fe7ddb2eda3867a71f4af748b
wkoslik <wkoslik@gmail.com>
2021-02-20 21:32:53 -0500

    moving like and dislike buttons

--- src/components/pages/Infocard.js
+++ src/components/pages/Infocard.js
@@ -1,8 +1,24 @@
 
 const Infocard = (props) => {
+  //TODO The buttons needed to move to the infocard.js file considering the restaurant info will be living there
+  const likeIt = () =>{
+    //TODO like it function
+    console.log("like it!")
+    //this will push the selected restaurant information to the DB
+    //this will render the next infoCard.
+  }
+
+  const dislikeIt = () =>{
+    //TODO dislike it function
+    console.log("DO NOT WANT!")
+    //this will push the selected restaurant information to the DB
+    //this will render the next infoCard.
+  }
 
   return (
     // TODO remove styling
+    <div id="info">        
+    <button onClick={dislikeIt}>❌</button>
     <div className="info-card">
       <h3>Restaurant name</h3>
       <img src="https://arlingtonva.s3.amazonaws.com/wp-content/uploads/sites/25/2013/12/restaurant.jpeg" alt="holding image" />
@@ -11,6 +27,8 @@ const Infocard = (props) => {
         <span>$$$</span>
         <span>⭐️⭐️⭐️⭐️</span>
       </div>
+    </div>
+    <button onClick={likeIt}>✅</button>
     </div>
   );
 }
--- src/components/pages/Restaurants.js
+++ src/components/pages/Restaurants.js
@@ -1,14 +1,13 @@
 import Infocard from './Infocard'
 
 const Restaurants = (props) => {
+//TODO The buttons needed to move to the infocard.js file considering the restaurant info will be living there
   return (
     <div>
       <h3>Restaurants page, user makes a choice here</h3>
       {/* TODO remove styling */}
       <div className="restaurants">
-        <button>❌</button>
         <Infocard />
-        <button>✅</button>
       </div>
       <p>user make choices above buttons, then move on to <a href="/notification">notice page</a></p>
     </div>
