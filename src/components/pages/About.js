7e6eef488e6225de6e2ffc67c556540595bd22f8
wkoslik <wkoslik@gmail.com>
2021-02-22 11:51:14 -0500

    footer delete

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -9,6 +9,12 @@ const About = (props) => {
         <li>Whitney, link to LinkedIn, link to resume</li>
         <li>Sean, link to LinkedIn, link to resume</li>
       </ul>
+      <div>
+      {/* TODO get images of everyone */}
+        <a href="https://www.linkedin.com/in/youngsang-kim/" target="_blank"><img src='' alt='Youngsang Kim' /></a>
+        <a href="https://www.linkedin.com/in/wkoslik/" target="_blank"><img src='' alt='Whitney Koslik' /></a>
+        <a href="https://www.linkedin.com/in/seanmichael-feiner/" target="_blank"><img src='' alt='Sean Feiner' /></a>
+      </div>
     </div>
   );
 }
